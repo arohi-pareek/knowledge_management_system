26f253e0f8ec03554fcf204449d56731506270ef
kritika915 <kritikasrivastava915@gmail.com>
2023-10-17 10:16:03 +0530

    change

--- src/Forms/components/footer.js
+++ src/Forms/components/footer.js
@@ -1,14 +1,41 @@
-import React from 'react'
-import '../style/Footer.css'
+import React from "react";
+import "./footer.css"
+
 const Footer = () => {
   return (
-    <div class="hover-area">
-    <div className="footer">
-     
-       <h5 className="f">© 2023 Educom-Let Us Study, Inc.</h5>
-   </div> 
-   </div>
+    <div className="AppBottom">
+          <div className="Footercontainer">
+            <div className="eOffice">
+              <p
+                style={{
+                  fontSize: "12px",
+                  position: "relative",
+                  color: "#fff",
+                  margin: "auto",
+                  marginTop: "5px",
+                  right: "10em",
+                }}
+              >
+                &reg; COSTA CLOUD
+              </p>
+            </div>
+            <div className="versions">
+              <p
+                style={{
+                  cursor: "pointer",
+                  color: "#fff",
+                  position: "relative",
+                  margin: "auto",
+                  marginTop: "5px",
+                }}
+                // onClick={() => setOpen(true)}
+              >
+                v1.1.1
+              </p>
+            </div>
+          </div>
+        </div>
   );
-}
+};
 
-export default Footer
+export default Footer;
